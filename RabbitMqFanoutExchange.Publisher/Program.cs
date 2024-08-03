2ba80b891d6531c3f5e5d3bfb05bc83e128198d3
Alihan Girgin <alihangir@gmail.com>
2024-08-04 01:40:21 +0300

    fanout exchange added

---
+++ RabbitMqFanoutExchange.Publisher/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqps://qnjazkgn:3M3LtCMl8P1lfGi8h-wm_cOp56ySrUW9@sparrow.rmq.cloudamqp.com/qnjazkgn")
+};
+
+var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+channel.ExchangeDeclare("logsFanout", ExchangeType.Fanout, true);
+
+
+for (int i = 0; i < 50; i++)
+{
+    var message = $"log{i}";
+    var messageBytes = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish("logsFanout", string.Empty, null, messageBytes); // Fanout için routing key kullanılmaz
+    Console.WriteLine($"Message Sent:{i}");
+
+}
+Console.ReadLine();
---
+++ RabbitMqFanoutExchange.Subscriber/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqps://qnjazkgn:3M3LtCMl8P1lfGi8h-wm_cOp56ySrUW9@sparrow.rmq.cloudamqp.com/qnjazkgn")
+};
+
+var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+channel.BasicQos(0, 1, false);
+channel.ExchangeDeclare("logsFanout", ExchangeType.Fanout, true);
+
+// Geçici bir kuyruk oluştur
+var queueName = channel.QueueDeclare().QueueName;
+// Kuyruğu exchange'e bağla
+channel.QueueBind(queueName, "logsFanout", string.Empty);
+
+var consumer = new EventingBasicConsumer(channel);
+
+Console.WriteLine("Logs listening");
+
+consumer.Received += (sender, eventArgs) =>
+{
+    var body = eventArgs.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Message received:{message}");
+    channel?.BasicAck(eventArgs.DeliveryTag, false);
+    Thread.Sleep(1500);
+};
+
+channel.BasicConsume("helloRabbitMq", false, consumer);
+
+Console.ReadLine();
