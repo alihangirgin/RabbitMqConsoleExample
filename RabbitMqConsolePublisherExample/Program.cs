957b45ef3f380e7ac96678f195dcd831f08cc789
Alihan Girgin <alihangir@gmail.com>
2024-08-03 19:56:45 +0300

    Add project files.

---
+++ RabbitMqConsolePublisherExample/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqps://qnjazkgn:3M3LtCMl8P1lfGi8h-wm_cOp56ySrUW9@sparrow.rmq.cloudamqp.com/qnjazkgn")
+};
+
+var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+channel.QueueDeclare("helloRabbitMq", true, false, false);
+
+for (int i = 0; i < 50; i++)
+{
+    var message = $"hello world{i}";
+    var messageBytes = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(string.Empty, "helloRabbitMq", null, messageBytes);
+    Console.WriteLine($"Message Sent:{i}");
+
+}
+Console.ReadLine();
---
+++ RabbitMqConsoleSubscriberExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqps://qnjazkgn:3M3LtCMl8P1lfGi8h-wm_cOp56ySrUW9@sparrow.rmq.cloudamqp.com/qnjazkgn")
+};
+
+var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+channel.BasicQos(0,1,false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (sender, eventArgs) =>
+{
+    var body = eventArgs.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Message received:{message}");
+    channel?.BasicAck(eventArgs.DeliveryTag, false);
+    Thread.Sleep(1500);
+};
+
+channel.BasicConsume("helloRabbitMq", false, consumer);
+
+Console.ReadLine();
